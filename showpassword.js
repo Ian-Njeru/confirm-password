c8f5dfeccbb083b4dcf53818823e34cf7582ca04
Ian-Njeru <74234445+Ian-Njeru@users.noreply.github.com>
2021-11-04 11:32:33 +0300

    Add files via upload

---
+++ showpassword.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("password");
+  var y = document.getElementById("confirmpassword")
+  if (x.type === "password" && y.type === "password") {
+    x.type = "text";
+    y.type = "text";
+  } else {
+    x.type = "password";
+    y.type = "password"
+  }
+}
